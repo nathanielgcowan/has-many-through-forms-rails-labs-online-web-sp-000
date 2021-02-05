21cb245878cd917da771729180cdfc5ec6a2956a
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-02-05 13:59:23 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,19 +14,3 @@ class Post < ActiveRecord::Base
     end
   end
 end
-
-
-has_many :post_categories
-has_many :categories, through: :post_categories
-has_many :comments
-has_many :users, through: :comments
-accepts_nested_attributes_for :categories
-
-def categories_attributes=(category_attributes)
-  category_attributes.values.each do |category_attribute|
-    if category_attribute["name"].present?
-      category = Category.find_or_create_by(category_attribute)
-      self.categories << category
-    end
-  end
-end
