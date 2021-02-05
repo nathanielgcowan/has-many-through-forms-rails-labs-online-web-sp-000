6080b0175b303f8d3e9d0d4448eeb6bbd51632f8
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-02-05 13:58:57 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,12 +5,28 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
 
+  #Avoid duplicates
   def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
-      if category_attribute["name"].present?
-        category = Category.find_or_create_by(category_attribute)
-        self.categories << category
-      end
+  category_attributes.values.each do |category_attribute|
+    if category_attribute["name"].present?
+    category = Category.find_or_create_by(category_attribute)
+    self.categories << category
+    end
+  end
+end
+
+
+has_many :post_categories
+has_many :categories, through: :post_categories
+has_many :comments
+has_many :users, through: :comments
+accepts_nested_attributes_for :categories
+
+def categories_attributes=(category_attributes)
+  category_attributes.values.each do |category_attribute|
+    if category_attribute["name"].present?
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
     end
   end
 end
