92b732f9d7d9bb6cdf78b96ef2795a97e06d1c8b
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-02-05 13:50:00 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,10 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user
 
+  def user_attributes=(user_attributes)
+  self_attributes.values.each do |user_attribute|
+    user = User.find_or_create_by(user_attribute)
+    self.user << user
+    end
+  end
 end
