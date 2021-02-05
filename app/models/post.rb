1dec446ae0d6e24778cf10d7bd1c1d48c6f1a03c
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-02-05 13:59:07 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
   def categories_attributes=(category_attributes)
   category_attributes.values.each do |category_attribute|
     if category_attribute["name"].present?
-    category = Category.find_or_create_by(category_attribute)
+      category = Category.find_or_create_by(category_attribute)
     self.categories << category
     end
   end
